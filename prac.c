7b92130b5c7cb3b856fa72e80e06dfdbba226ad2
olojedesuliatcodes <temitayoabiodun15@gmail.com>
2025-02-07 20:03:40 -0800

    i created a prac.c file with vim in my cprogramming repository,where i wrote a code practice on operator precedence

---
+++ prac.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+		int z = 20;
+		int x;
+		int y;
+
+	        y = 10;
+	        x = -y + z;
+                //x = (-y) + z; x = 10
+
+                printf("%1\n",x);
+		
+		return 0;
+
+}
